3b461f1ed0ecf6afdbdb070d991b40111b803532
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-04-25 19:30:32 -0300

    aula2

--- aula02/04_desafio_3.py
+++ aula02/04_desafio_3.py
@@ -1,9 +1,16 @@
-from datetime import datetime, timedelta
+from datetime import datetime, date, time, timedelta
 
 # Peça para o usuário digitar a data de seu próximo aniversário no formato brasileiro
+aniversario = input('Digite sua data de aniversário(dd/mm/aaaa)')
+aniversario_datetime = datetime.strptime(aniversario, '%d/%m/%Y')
+print(f'Seu aniversário é dia {str(aniversario_datetime)}')
 
 # Imprima uma mensagem dizendo quantos dias faltam para o aniversário dele
+hoje = datetime.now().date()
+print('Hoje é: ' + hoje.strftime('%d/%m/%y'))
 
+contagem = hoje.strftime('%d/%m/%y') - aniversario_datetime
+print('Faltam ' + contagem.strftime('%d') + ' dias para o seu aniversário.')
 # Pergunte ao usuário se ele(a) gosta de aniversário e salve a resposta
 
 # Pergunte ao usuário se ele(a) vai fazer festa e salve a resposta
