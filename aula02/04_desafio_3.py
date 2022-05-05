b217d63019fd633da1f489538bf9e224feea2ccc
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-05-05 19:29:44 -0300

    certificado operadores matematicos e desafio 4

--- aula02/04_desafio_3.py
+++ aula02/04_desafio_3.py
@@ -1,20 +1,26 @@
 from datetime import datetime, date, time, timedelta
 
 # Peça para o usuário digitar a data de seu próximo aniversário no formato brasileiro
-aniversario = input('Digite sua data de aniversário(dd/mm/aaaa)')
+aniversario = input('Digite a data do seu próximo aniversário:(dd/mm/aaaa)')
 aniversario_datetime = datetime.strptime(aniversario, '%d/%m/%Y')
 print(f'Seu aniversário é dia {str(aniversario_datetime)}')
 
 # Imprima uma mensagem dizendo quantos dias faltam para o aniversário dele
-hoje = datetime.now().date()
-print('Hoje é: ' + hoje.strftime('%d/%m/%y'))
+hoje = datetime.now()
+print(f"Hoje é: {hoje}")
+contagem = aniversario_datetime - hoje
+print(f"Faltam {contagem.days} dias para seu aniversário!")
 
-contagem = hoje.strftime('%d/%m/%y') - aniversario_datetime
-print('Faltam ' + contagem.strftime('%d') + ' dias para o seu aniversário.')
 # Pergunte ao usuário se ele(a) gosta de aniversário e salve a resposta
+resposta_aniversario = input("Você gosta do seu aniversário?(s/n)")
 
 # Pergunte ao usuário se ele(a) vai fazer festa e salve a resposta
+resposta_festa = input("Você vai fazer uma festa no seu aniversário?(s/n)")
 
 # Imprima uma mensagem dizendo se o usuário vai ganhar presente ou não
 # A regra é: o usuário só pode ganhar presente se gostar de aniversário e for fazer festa.
+if resposta_aniversario == "s" and resposta_festa == "s":
+  print("Você ganhará um Presente!!!")
+else:
+  print("Você não vai ganhar um presente.")
 
