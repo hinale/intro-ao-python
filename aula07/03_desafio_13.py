592ceed34192305f8ddd4968301842575b9d07e3
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-05-05 22:01:30 -0300

    certificado logica booliana

--- aula07/03_desafio_13.py
+++ aula07/03_desafio_13.py
@@ -1,11 +1,16 @@
 import time
 
 # Crie um decorator que calcule o tempo de execução de uma determinada função
-
+def logger(func):
+    def wrapper(x, y):
+        print(f'Começando a função {func.__name__}')
+        func(x, y)
+        print(f'Terminando a função {func.__name__}')
+    return wrapper
 
 
 # Aplique seu decorator na função abaixo e veja quanto tempo a busca de um mesmo valor em um set e uma lista demoram para serem executadas
-
+@logger
 def encontrar_item(container, item):
     return item in container
 
