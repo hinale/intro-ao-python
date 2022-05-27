a937b70f1ef572854dd27ad0bc451242b4b1526a
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-05-27 16:04:08 -0300

    desafio 11 e 12 da aula06

--- aula06/04_desafio_11.py
+++ aula06/04_desafio_11.py
@@ -3,4 +3,21 @@
 # Exemplo de entrada: Oi mulheres maravilhosas do curso de Python do ConstruDelas!
 # Exemplo de saída: !saleDurtsnoC od nohtyP ed\nosruc od sasohlivaram serehlum iO
 
+from distutils import archive_util
+
+
 arquivo = 'entrada_desafio_11.txt'
+
+def ler_arquivo(entrada):
+    with open(entrada, 'r') as arquivo:
+        conteudo = arquivo.read()
+        print(conteudo)
+        
+def escrever_arquivo(entrada):
+    with open(entrada, 'a+') as arquivo:
+        arquivo.write('\n!saleDurtsnoC od nohtyP ed osruc od sasohlivaram serehlum iO')
+        return arquivo
+
+ler_arquivo(arquivo)
+escrever_arquivo(arquivo)
+ler_arquivo(arquivo)
--- aula06/08_desafio_12.py
+++ aula06/08_desafio_12.py
@@ -1,3 +1,15 @@
 # Escreva um programa que leia o arquivo "exemplo2.csv", e converta
 # os registros desse CSV para o formato JSON. Escreva um arquivo de saída
 # que contenha o conteúdo em JSON.
+import csv
+import json
+
+with open('exemplo2.csv', mode='r') as arquivo:
+    leitor_csv = csv.DictReader(arquivo)
+    alunos = []
+    for registro in leitor_csv:
+        alunos.append(registro)
+
+alunos_json = json.dumps(alunos)
+
+print(alunos_json)
